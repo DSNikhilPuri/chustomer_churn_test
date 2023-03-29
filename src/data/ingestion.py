6e674fd3351d2c5ab51cf65b5207103baf026155
DSNikhilPuri <114741441+DSNikhilPuri@users.noreply.github.com>
2023-03-29 14:40:30 +0530

    data eda

---
+++ src/data/ingestion.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import sweetviz as sv
+import os
+
+
+df = pd.read_excel("C:\\Users\\nikhil.puri\\OneDrive - Marico Ltd\\Desktop\\customer_churn\\chustomer_churn_test\\data\\external\\capstone_project-telecom_churn.xlsx", sheet_name=0)
+
+my_report = sv.analyze(df)
+my_report.show_html(filepath='C:\\Users\\nikhil.puri\\OneDrive - Marico Ltd\\Desktop\\customer_churn\\chustomer_churn_test\\reports\\eda.html')
